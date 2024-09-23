70d6b808c49b39ea3eb165c98871382c59bf522e
khaledHassan2 <khaledamalb22@gmail.com>
2024-09-24 00:39:32 +0300

    Edit design login ,register and navbar

--- NewsHup/wwwroot/js/site.js
+++ NewsHup/wwwroot/js/site.js
@@ -2,3 +2,37 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+$(document).ready(function () {
+    $(".owl-carousel").owlCarousel();
+});
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    autoplay: true,
+    autoplayHoverPause: true,
+    autoplayTimeout: 1500,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 700,
+    navText: ['', ''],
+    items: 1,
+    nav: true,
+    rtl: true,
+});
+$('.play').on('click', function () {
+    owl.trigger('play.owl.autoplay', [1000])
+})
+$('.stop').on('click', function () {
+    owl.trigger('stop.owl.autoplay')
+})
+$(document).ready(function () {
+
+    const navbarLinks = $('.navbar .main-ul a');
+
+    navbarLinks.on('click', function () {
+        navbarLinks.removeClass('active');
+        $(this).addClass('active');
+    });
+});
