eddcac3c03b4fa4efe88cee838c8cc972d3d6170
Hassan Ezzeldien <hassanezz280@gmail.com>
2024-10-03 09:09:11 +0200

    Add :  Add users [Admin Dashboard]

--- NewsHup/Controllers/AdminController.cs
+++ NewsHup/Controllers/AdminController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NewsHup.Models;
 using TestMVC.Models;  // Assuming NewsContext is in this namespace
 
 namespace NewsHup.Controllers
@@ -25,6 +26,21 @@ namespace NewsHup.Controllers
         {
             var users = _context.Users.ToList(); // Fetch all users from the DB
             return View(users);  // Pass the list of users to the view
+        }
+        // This will handle the POST request to add a new user
+        [HttpPost]
+        public IActionResult AddUser(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                // Add the user to the database
+                _context.Users.Add(user);
+                _context.SaveChanges();
+
+                return Json(new { success = true, message = "User added successfully!" });
+            }
+            // If the data is invalid, return an error response
+            return Json(new { success = false, message = "Invalid data!" });
         }
     }
 }
